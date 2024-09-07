7d2fa0c521a4ff58acc7373b6e8851b6557a47a2
danmachooo <johnpauldanmachi@gmail.com>
2024-09-07 16:42:16 +0800

    Added 2 Function

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const queryString = require('querystring');
+
+//Filepath
+const rantFilePath = path.join(__dirnamem, 'data', 'rants.json');
+
+//Read rants
+function readRants() {
+    try {
+        const data = fs.readFileSync(rantFilePath, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+}
+
+function writeRants(rants){
+    fs.writeFileSync(rantFilePath, JSON.stringify(rants, null, 2), 'utf8');
+}
+
+function serveFile(res, filePath, contentType) {
+    fs.readFile(filePath, 'utf8', (err, content) => {
+        if(err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'})
+        }
+    })
+}
+
